3b6f0c27a2d6ff4f5c905bb057476a49c3e493dd
Samco <adeyemisamuela@gmail.com>
2022-12-18 04:39:34 +0100

    add any changes or files

--- users/forms.py
+++ users/forms.py
@@ -10,11 +10,7 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2' ]
-
-
-#class UserUpdateForm (forms.ModelForm):
-    #email = forms.EmailField()
-    
+        
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
