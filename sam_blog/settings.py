fd9ccc96d86bf4d4a6f42c36f469cdd3f27f1cb1
Samuel Ayobami ADEYEMI <46057925+Samco2@users.noreply.github.com>
2023-02-10 13:38:17 +0100

    Start stylying

--- sam_blog/settings.py
+++ sam_blog/settings.py
@@ -135,20 +135,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
-#EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-#EMAIL_HOST= 'smtp.gmail.com'
-#EMAIL_PORT = 587
-#EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-#EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_HOST_USER = 'samcoadeyemi@outlook.com'
-EMAIL_HOST_PASSWORD = 'Isakunle1/'
+EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER_DJANGO')
+EMAIL_HOST_PASSWORD = 'nsvoqmfidltjprfa'
+EMAIL_USE_TLS = True
+
+# EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp-mail.outlook.com'
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER_OUTLOOK')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS_OUTLOOK')
+# EMAIL_PORT = 25
 
 
-#EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-#EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+
+
